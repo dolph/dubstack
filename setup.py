f5cc64d5ab870f43aaf82158e43913c21052a979
Dolph Mathews <dolph.mathews@gmail.com>
2011-11-16 14:14:38 -0800

    Updated setup.py to reflect renaming

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
-setup(name='keystonelight',
+setup(name='dubstack',
       version='1.0',
-      description="Authentication service for OpenStack",
+      description="Dubstep karaoke service for OpenStack",
       author='OpenStack, LLC.',
       author_email='openstack@lists.launchpad.net',
       url='http://www.openstack.org',
       packages=find_packages(exclude=['test', 'bin']),
-      scripts=['bin/keystone', 'bin/ksl'],
+      scripts=['bin/dubstack', 'bin/dubstack-manage'],
       zip_safe=False,
       install_requires=['setuptools'],
       )
